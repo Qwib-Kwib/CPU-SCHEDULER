5898225205fea57cf7694ab0d2091eef4d79a2db
Qwib-Kwib <marcpantinos@gmail.com>
2024-09-25 21:29:16 +0800

    test

--- universitydb.sql
+++ universitydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2024 at 02:15 PM
+-- Generation Time: Sep 25, 2024 at 02:15 PM bruh
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
